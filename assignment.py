46da8f44c2ce9c8bf6bf4342488c6ebe543e61e5
sarvnazale <sarvnazale@gmail.com>
2020-03-24 20:55:52 -0400

    Final copy

--- assignment.py
+++ assignment.py
@@ -1,3 +1,7 @@
-name = "Sarvnaz"
-address = "L4B0A2 - 23 Oneida Crescent"
-print(name, address)
+#
+Line_1 = "Sarvnaz Alemohammad"
+Line_2 = "402 - 23 Oneida Crescent"
+Line_3 = "Richmond Hill ON  L4B0A2"
+print(Line_1)
+print(Line_2)
+print(Line_3)
